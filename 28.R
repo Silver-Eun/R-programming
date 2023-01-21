d1fa49a8f0de5853c2e87e22e0646c3180836c60
Silver-Eun <h22sang@naver.com>
2023-01-21 21:13:39 +0900

    googlemap5-1

--- 28.R
+++ 28.R
@@ -23,3 +23,25 @@ treemap(GNI2014,index=c('continent','iso3'),vsize='population',
 
 treemap(GNI2014,index=c('continent','iso3'),vsize='population',
         vcolor='GNI',type='index')
+
+data(business)
+str(business)
+
+business
+
+treemap(business, index=c('NACE1','NACE2','NACE3'),
+        vsize='turnover',type='index')
+
+write.csv(business,'~/business.csv')
+
+treemap(business[business$NACE1=='F - Construction',],
+        index=c('NACE2','NACE3'),vsize='employee',type='index')
+
+treemap(business[business$NACE1=='F - Construction',],
+        index=c('NACE2','NACE3'),vsize='employee',type='value')
+
+treemap(business[business$NACE1=='F - Construction',],
+        index=c('NACE2','NACE3'),vcolor='employee',vsize='employees',type='index')
+
+treemap(business[business$NACE1=='F - Construction',],
+        index=c('NACE2','NACE3'),vcolor='turnover',vsize='employees',type='index')
