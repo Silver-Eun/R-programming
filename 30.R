4ba2276f249d5d46f1070fb6cb3ca17a457e7bef
Silver-Eun <h22sang@naver.com>
2023-04-12 22:16:52 +0900

    using csv2

--- 30.R
+++ 30.R
@@ -43,3 +43,31 @@ as.double(xList)
 # s = simplify
 
 sapply(x, mean)
+
+lapply(x, function(y)(y+1))
+
+sapply(x, function(y){y+1})
+
+vapply(x, mean, double(1))
+
+vapply(x, function(y){y+1}, double(5))
+
+vapply(x, function(y){y+1}, double(10))
+
+a<-matrix(rnorm(200),20,10)
+lapply(a,mean)
+
+apply(a,2,mean)
+
+apply(a,2,mean) # average over column
+apply(a,1,mean) # average over row
+
+colMeans(a) # Mean over column
+colSums(a) # sum over column
+
+rowMeans(a) # Mean over row
+rowSums(a) # sum over row
+
+# tapply
+
+x<-c(rnorm(10),runif(10),rnorm(10,1))
